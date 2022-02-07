58e40ba968ad436bfb06af1ace16fd7446321aee
fredlapolla <fred.lapolla@med.nyu.edu>
2022-02-07 10:31:00 -0500

    Add files via upload

---
+++ RCrashCourseGroupWork_Feb22.R
@@ -0,0 +1,15 @@
+#MHPE Group Work
+
+#1. Exercise: Reclassify DX_DBTS, a variable that holds three groups, 1) Diabetes with a diagnosis; 2) Diabetes with no diagnosis and 3) No Diabetes. 
+
+
+
+#2. Exercise: What type of structure is nyc?
+
+
+
+#3. Exercise: Create a visualization to assess the skew of LEAD and of HDL levels.
+
+
+
+#4. Exercise: Create a subset of the nyc_hanes dataset that has data only for the oldest group of diabetics that have a total cholesterol over 200. How many observations are in the resulting dataset?
